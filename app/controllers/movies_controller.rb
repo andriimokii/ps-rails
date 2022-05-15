dbcc10ac6d5bcc5e8e0579c90c5ba3ea147e8ac9
Andrii Mokii <andriimokii@gmail.com>
2022-05-15 22:33:54 +0300

    15. CRUD

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,47 @@ class MoviesController < ApplicationController
         @movies = Movie.all
         # @movies = ["Iron Man", "Superman", "Spider-Man", "Dog"]
     end
+
+    def show
+        @movie = Movie.find(params[:id])
+    end
+
+    def edit
+        @movie = Movie.find(params[:id])
+    end
+
+    def update
+        # fail
+        @movie = Movie.find(params[:id])
+        @movie.update(movie_params)
+
+        redirect_to @movie
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        # fail
+        @movie = Movie.create(movie_params)
+
+        redirect_to @movie 
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+
+        redirect_to(movies_url, status: :see_other)
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie)
+            .permit(:title, :description, :rating, :released_on, :total_gross)
+    end
+
+
 end
