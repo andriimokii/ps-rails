d54fe41aae44cdd0fecc3222a9f7fb575a4d2528
Andrii Mokii <andriimokii@gmail.com>
2022-06-03 00:18:58 +0300

    Change email to Bussiness

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationsMailer < ApplicationMailer
-  default from: 'andriimokii@gmail.com'
+  default from: 'x1c.mailer@gmail.com'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
